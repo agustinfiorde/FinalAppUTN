c2280d5d54c0c062e4033cdb2cd254d6b0ba2823
agustinfiorde <agulirio@yahoo.com.ar>
2020-10-24 18:33:39 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/about/about.component';
+import { DogListComponent } from './components/dog/dog-list/dog-list.component';
+import { DogDetailsComponent } from './components/dog/dog-details/dog-details.component';
+import { DogFormComponent } from './components/dog/dog-form/dog-form.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductFormComponent } from './components/product/product-form/product-form.component';
+import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user/user-details/user-details.component';
+import { UserFormComponent } from './components/user/user-form/user-form.component';
+import { ErrorComponent } from './components/error/error.component';
+import { MessageComponent } from './components/message/message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: 'message', component: MessageComponent },
+  { path: 'dogs', component: DogListComponent },
+  { path: 'dog/:id', component: DogDetailsComponent },
+  { path: 'dog/register', component: DogFormComponent },
+
+
+  { path: 'users', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailsComponent },
+  { path: 'user/register', component: UserFormComponent },
+
+
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
+  { path: 'product/register', component: ProductFormComponent },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
+  // imports: [RouterModule.forRoot(routes, { useHash: true })],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const RoutingComponents = [];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,52 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { UserFormComponent } from './components/user/user-form/user-form.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { UserPageComponent } from './components/user/user-page/user-page.component';
+import { DogFormComponent } from './components/dog/dog-form/dog-form.component';
+import { DogListComponent } from './components/dog/dog-list/dog-list.component';
+import { DogDetailsComponent } from './components/dog/dog-details/dog-details.component';
+import { UserDetailsComponent } from './components/user/user-details/user-details.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { ProductFormComponent } from './components/product/product-form/product-form.component';
+import { ProductPageComponent } from './components/product/product-page/product-page.component';
+import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
+import { DogPageComponent } from './components/dog/dog-page/dog-page.component';
+import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainComponent,
+    LoginComponent,
+    ErrorComponent,
+    AboutComponent,
+    ContactComponent,
+    UserFormComponent,
+    UserListComponent,
+    UserPageComponent,
+    DogFormComponent,
+    DogListComponent,
+    DogDetailsComponent,
+    UserDetailsComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductPageComponent,
+    ProductDetailsComponent,
+    DogPageComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
